739ed1bdecb025f6d6bf2701c12671d609269ac4
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-06-19 10:07:47 -0700

    Adds mongoose models

---
+++ server/models/Category.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  products: [
+    {
+      type: String
+    }
+  ]
+})
+
+module.exports = mongoose.model("category", CategorySchema);
